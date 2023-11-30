3ed475cf2b3e3acd239843aa5ed01418e45f011d
nadeyabuiko <150372880+nadeyabuiko@users.noreply.github.com>
2023-11-30 15:26:26 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_lenguages(browser):
+def test_languages(browser):
     browser.get(link)
     browser.implicitly_wait(5)
     button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")

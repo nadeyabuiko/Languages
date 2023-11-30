73db4571e79d6772be873d1b9b93b211c58a9328
nadeyabuiko <150372880+nadeyabuiko@users.noreply.github.com>
2023-11-30 15:25:45 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_lenguages(browser):
     browser.get(link)
     browser.implicitly_wait(5)
     button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
